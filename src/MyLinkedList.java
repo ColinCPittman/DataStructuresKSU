7f30dcd87a00cee80915ee6372d33a1b18ba69d5
ColinCPittman <Colincpittman@gmail.com>
2024-08-24 14:21:28 -0400

    still fleshing out my custom linked list implementation in anticipation of the upcoming bonus question

--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -5,59 +5,129 @@ import java.util.function.Consumer;
 public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
 
     private class Node<E> {
-        E element;
+        E data;
         Node<E> next;
 
-        public Node(E element) {
-            this.element = element;
+        public Node(E data) {
+            this.data = data;
         }
 
-        public Node(E element, Node<E> next) {
-            this.element = element;
+        public Node(E data, Node<E> next) {
+            this.data = data;
             this.next = next;
         }
     }
 
     private Node<E> head;
     private Node<E> tail;
+    private int size;
 
-    public MyLinkedList() {
-        // Constructor logic (if any)
+    public MyLinkedList() { //Default constructor
+        head = null;
+        tail = null;
+        size = 0;
     }
 
     public MyLinkedList(E[] elements) {
         // Constructor with elements array
+        for (E element : elements) {
+
+        }
     }
 
     public void addFirst(E e) {
-        head = new Node<>(e); // Use <E> for the Node type
-        tail = head;
+        Node<E> newNode = new Node<>(e);
+        if (head == null) {
+            head = newNode; // Use <E> for the Node type
+            tail = head;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
         size++;
     }
 
     public void addLast(E e) {
         // Add element at the end of the list
+        if (head == null) {
+            addFirst(e);
+        }
+        tail.next = new Node<>(e);
+        size++;
     }
 
     public E getFirst() {
-        return null; // Return the first element
+        if (head == null) {
+            throw new UnderflowException("List is empty, cannot retrieve the first element.");
+        }
+        return head.data; // Return the first element
     }
 
     public E getLast() {
-        return null; // Return the last element
+        if (head == null) { //since it is enforced that tail == null iff head == null,  I am checking for head == null here for logical consistency across the class.
+            throw new UnderflowException("List is empty, cannot retrieve the last element.");
+        }
+        return tail.data; // Return the last element
     }
 
     public E removeFirst() {
-        return null; // Remove and return the first element
+        // Remove and return the first element
+        if (head == null) {
+            throw new UnderflowException("List is empty, cannot remove the first element.");
+        }
+        Node<E> deleted = head;
+        head = head.next;
+        size--;
+        if (head == null) {
+            tail = null;
+        }
+        return deleted.data;
     }
 
     public E removeLast() {
-        return null; // Remove and return the last element
+        // Return and remove the last element
+        if (head == null) { //since it is enforced that tail == null iff head == null,  I am checking for head == null here for logical consistency across the class.
+            throw new UnderflowException("List is empty, cannot remove the last element.");
+        }
+
+        if (head == tail) {
+            Node<E> last = head;
+            head = null;
+            tail = null;
+            size--;
+            return head.data;
+        }
+        Node<E> previous = head;
+        Node<E> current = head.next;
+        while (current.next != null) {
+            previous = previous.next;
+            current = current.next;
+        }
+        tail = previous;
+        tail.next = null;
+        size--;
+        return current.data;
     }
 
     @Override
-    public void add(int index, E e) { // Corrected method signature
-        // Implementation for adding element at specified index
+    public void add(int index, E e) {
+        if (index > size || index < 0) {
+            throw new OverflowException("Index out of bounds for list of size " + size + ".");
+        }
+        if (index == 0) {
+            addFirst(e);
+            return;
+        }
+        if (index == size) {
+            addLast(e);
+            return;
+        }
+        Node<E> previous = head;
+        for (int i = 0; i < index - 1; i++) {
+            previous = previous.next;
+        }
+        previous.next = new Node<E>(e, previous.next);
+        size++;
     }
 
     @Override
