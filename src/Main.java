bdd86e71a7000fd2644fc76b121cab4c8b6e6972
ColinCPittman <colincpittman@gmail.com>
2024-08-26 13:25:54 -0400

    Methods seem to be working on initial testing. Will need to do further testing after bonus question is posted.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,147 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        while (true) {
+            System.out.println(menu + list.toString());
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter element to add at the beginning: ");
+                    int firstElement = scanner.nextInt();
+                    list.addFirst(firstElement);
+                    System.out.println("Added " + firstElement + " at the beginning.");
+                    break;
+                case 2:
+                    System.out.print("Enter element to add at the end: ");
+                    int lastElement = scanner.nextInt();
+                    list.addLast(lastElement);
+                    System.out.println("Added " + lastElement + " at the end.");
+                    break;
+                case 3:
+                    try {
+                        System.out.println("First element: " + list.getFirst());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 4:
+                    try {
+                        System.out.println("Last element: " + list.getLast());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 5:
+                    try {
+                        System.out.println("Removed first element: " + list.removeFirst());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 6:
+                    try {
+                        System.out.println("Removed last element: " + list.removeLast());
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 7:
+                    System.out.print("Enter index to get element: ");
+                    int indexToGet = scanner.nextInt();
+                    try {
+                        System.out.println("Element at index " + indexToGet + ": " + list.get(indexToGet));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 8:
+                    System.out.print("Enter index to add element: ");
+                    int indexToAdd = scanner.nextInt();
+                    System.out.print("Enter element to add: ");
+                    int elementToAdd = scanner.nextInt();
+                    try {
+                        list.add(indexToAdd, elementToAdd);
+                        System.out.println("Added element " + elementToAdd + " at index " + indexToAdd);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 9:
+                    System.out.print("Enter index to remove element: ");
+                    int indexToRemove = scanner.nextInt();
+                    try {
+                        System.out.println("Removed element: " + list.remove(indexToRemove));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 10:
+                    System.out.print("Enter index to set element: ");
+                    int indexToSet = scanner.nextInt();
+                    System.out.print("Enter new element: ");
+                    int elementToSet = scanner.nextInt();
+                    try {
+                        System.out.println("Replaced element at index " + indexToSet + " with " + elementToSet + ". Old element was " + list.set(indexToSet, elementToSet));
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 11:
+                    System.out.print("Enter element to check if it exists: ");
+                    int elementToCheck = scanner.nextInt();
+                    if (list.contains(elementToCheck)) {
+                        System.out.println("List contains " + elementToCheck);
+                    } else {
+                        System.out.println("List does not contain " + elementToCheck);
+                    }
+                    break;
+                case 12:
+                    System.out.print("Enter element to find its first index: ");
+                    int elementForFirstIndex = scanner.nextInt();
+                    System.out.println("First index of element " + elementForFirstIndex + ": " + list.indexOf(elementForFirstIndex));
+                    break;
+                case 13:
+                    System.out.print("Enter element to find its last index: ");
+                    int elementForLastIndex = scanner.nextInt();
+                    System.out.println("Last index of element " + elementForLastIndex + ": " + list.lastIndexOf(elementForLastIndex));
+                    break;
+                case 14:
+                    MyLinkedList<Integer> copiedList = list.copyList();
+                    System.out.print("Copied List: ");
+                    copiedList.forEach(e -> System.out.print(e + " "));
+                    System.out.println();
+                    break;
+                case 15:
+                    list.clear();
+                    System.out.println("List cleared.");
+                    break;
+                case 0:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    return;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+            System.out.println();
+
+
     }
+
+    }
+    static Scanner scanner = new Scanner(System.in);
+    static MyLinkedList<Integer> list = new MyLinkedList<>();
+    static final String menu = """
+                Linked List Function Testing
+                1. Add First\t\t\t\t9. Remove Element at Index
+                2. Add Last\t\t\t\t\t10. Set Element at Index
+                3. Get First\t\t\t\t11. Check if List Contains Element
+                4. Get Last\t\t\t\t\t12. Get Index of First Occurrence
+                5. Remove First\t\t\t\t13. Get Index of Last Occurrence
+                6. Remove Last\t\t\t\t14. Copy List
+                7. Get Element by Index\t\t15. Clear List
+                8. Add Element at Index\t\t0. Exit\n
+                Current List Contents: """;
 }
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,8 +1,11 @@
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 import java.util.Spliterator;
 import java.util.function.Consumer;
 
-public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
+public class MyLinkedList<E> extends MyAbstractList<E> implements Iterable<E> {
+    private int modCount; // <E> added here
 
     private class Node<E> {
         E data;
@@ -25,25 +28,63 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
     public MyLinkedList() { //Default constructor
         head = null;
         tail = null;
+        modCount = 0;
         size = 0;
+    }
+
+    public MyLinkedList(E element) {
+        addFirst(element);
     }
 
     public MyLinkedList(E[] elements) {
         // Constructor with elements array
         for (E element : elements) {
-
+            addFirst(element);
         }
     }
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
 
+        Node<E> current = head;
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) {
+                sb.append(", ");
+            }
+            current = current.next;
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
+    public MyLinkedList copyList() {
+        if(head == null) {
+            return new MyLinkedList();
+        }
+        if(size == 1) {
+            return new MyLinkedList(head.data);
+        }
+        MyLinkedList<E> newList = new MyLinkedList<>(head.data);
+        Node<E> current = head.next;
+        while(current != null) {
+            newList.addLast(current.data);
+            current = current.next;
+        }
+        return newList;
+    }
     public void addFirst(E e) {
         Node<E> newNode = new Node<>(e);
         if (head == null) {
             head = newNode; // Use <E> for the Node type
             tail = head;
-        } else {
+        }
+        else {
             newNode.next = head;
             head = newNode;
         }
+        modCount++;
         size++;
     }
 
@@ -53,6 +94,8 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
             addFirst(e);
         }
         tail.next = new Node<>(e);
+        tail = tail.next;
+        modCount++;
         size++;
     }
 
@@ -60,6 +103,7 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
         if (head == null) {
             throw new UnderflowException("List is empty, cannot retrieve the first element.");
         }
+
         return head.data; // Return the first element
     }
 
@@ -67,6 +111,7 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
         if (head == null) { //since it is enforced that tail == null iff head == null,  I am checking for head == null here for logical consistency across the class.
             throw new UnderflowException("List is empty, cannot retrieve the last element.");
         }
+
         return tail.data; // Return the last element
     }
 
@@ -75,12 +120,16 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
         if (head == null) {
             throw new UnderflowException("List is empty, cannot remove the first element.");
         }
+
         Node<E> deleted = head;
         head = head.next;
+        modCount++;
         size--;
+
         if (head == null) {
             tail = null;
         }
+
         return deleted.data;
     }
 
@@ -97,86 +146,283 @@ public class MyLinkedList<E> extends MyAbstractList<E> { // <E> added here
             size--;
             return head.data;
         }
+
         Node<E> previous = head;
         Node<E> current = head.next;
+
         while (current.next != null) {
             previous = previous.next;
             current = current.next;
         }
+
         tail = previous;
         tail.next = null;
+        modCount++;
         size--;
         return current.data;
     }
 
     @Override
     public void add(int index, E e) {
-        if (index > size || index < 0) {
-            throw new OverflowException("Index out of bounds for list of size " + size + ".");
+        if (index >= size || index < 0) {
+            throw new OverflowException("Index of size " + index + " is not valid for zero-indexed list of size " + size + ".");
         }
+
         if (index == 0) {
             addFirst(e);
             return;
         }
-        if (index == size) {
+
+        if (index == size - 1) {
             addLast(e);
             return;
         }
+
         Node<E> previous = head;
         for (int i = 0; i < index - 1; i++) {
             previous = previous.next;
         }
+
         previous.next = new Node<E>(e, previous.next);
+        modCount++;
         size++;
     }
 
     @Override
     public void clear() {
         // Clear the list
+        head = null;
+        tail = null;
+        modCount++;
+        size = 0;
     }
 
     @Override
     public boolean contains(Object o) {
-        return false; // Check if the list contains the element
+        Node<E> current = head;
+
+        while (current != null) {
+            if (current.data.equals(o)) {
+                return true;
+            }
+            current = current.next;
+        }
+
+        return false;
+
     }
 
     @Override
     public E get(int index) {
-        return null; // Get the element at the specified index
+        if (head == null) return null;
+
+        if (index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index of size " + index + " is not valid for zero-indexed list of size " + size + ".");
+        }
+        if (index == size - 1) {
+            return tail.data;
+        }
+        if (index == 0) {
+            return head.data;
+        }
+        Node<E> current = head.next;
+        for (int i = 0; i < index - 1; i++) {
+            current = current.next;
+        }
+        return current.data;
     }
+
 
     @Override
     public int indexOf(Object o) {
-        return 0; // Get the index of the first occurrence of the element
+        // Get the index of the first occurrence of the element
+        int index = 0;
+        Node<E> current = head;
+
+        while (current != null) {
+
+            if (current.data.equals(o)) {
+                return index;
+            }
+
+            current = current.next;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public int lastIndexOf(Object o) {
-        return 0; // Get the index of the last occurrence of the element
+       // Get the index of the last occurrence of the element
+        int index = 0;
+        int indexOfLastMatch = -1;
+        Node<E> current = head;
+
+        while (current != null) {
+
+            if (current.data.equals(o)) {
+                indexOfLastMatch = index;
+            }
+
+            current = current.next;
+            index++;
+        }
+        return indexOfLastMatch;
     }
 
     @Override
     public E remove(int index) {
-        return null; // Remove the element at the specified index
+        // Remove the element at the specified index
+        if(head == null) {
+            throw new UnderflowException("Cannot remove from empty list.");
+        }
+        if(index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index of size " + index + " is not valid for zero-indexed list of size " + size + ".");
+        }
+        if (index == 0) {
+            return removeFirst();
+        }
+        if(index == size -1) {
+            return removeLast();
+        }
+
+        Node<E> previous = head;
+        Node<E> current = head.next;
+
+        for (int i = 0; i < index - 1; i++) {
+            previous = previous.next;
+            current = current.next;
+        }
+        previous.next = current.next;
+        size--;
+        modCount++;
+        return current.data;
     }
 
     @Override
-    public E set(int index, E e) { // Corrected method signature
-        return null; // Replace the element at the specified index and return the old element
+    public E set(int index, E e) {
+        // Replace the element at the specified index and return the old element
+        if(head == null) {
+            throw new IndexOutOfBoundsException("Cannot set elements on empty list.");
+        }
+        if(index >= size || index < 0) {
+            throw new IndexOutOfBoundsException("Index of size " + index + " is not valid for zero-indexed list of size " + size + ".");
+        }
+        Node<E> current = head;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        E oldData = current.data;
+        current.data = e;
+        modCount++;
+        return oldData;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null; // Return an iterator over the elements in the list
+        return new LinkedListIterator();
     }
+    private class LinkedListIterator implements Iterator<E> {
+        private Node<E> current = head;
+        private int expectedModCount = modCount;
+        private Node<E> lastReturned = null;
 
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public E next() {
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            lastReturned = current;
+            current = current.next;
+            return lastReturned.data;
+        }
+
+        @Override
+        public void remove() {
+            if (lastReturned == null) {
+                throw new IllegalStateException();
+            }
+            if (modCount != expectedModCount) {
+                throw new ConcurrentModificationException();
+            }
+            // Handle removal when the last returned node is the head
+            if (lastReturned == head) {
+                removeFirst();
+            } else {
+                // Find the node before lastReturned
+                Node<E> previous = head;
+                while (previous.next != lastReturned) {
+                    previous = previous.next;
+                }
+                previous.next = lastReturned.next;
+                if (lastReturned == tail) { // If we're removing the tail
+                    tail = previous;
+                }
+                size--;
+                modCount++;
+                expectedModCount++;
+            }
+            lastReturned = null;
+        }
+    }
     @Override
     public void forEach(Consumer<? super E> action) {
-        super.forEach(action); // Perform the given action for each element of the list
+        Node<E> current = head;
+        while (current != null) {
+            action.accept(current.data);
+            current = current.next;
+        }
     }
 
     @Override
     public Spliterator<E> spliterator() {
-        return super.spliterator(); // Return a Spliterator over the elements in the list
+        return new LinkedListSpliterator(head, size, modCount); // Return a Spliterator over the elements in the list
+    }
+    private class LinkedListSpliterator implements Spliterator<E> {
+        private Node<E> current;
+        private int est; // size estimate
+        private int expectedModCount;
+
+        LinkedListSpliterator(Node<E> origin, int size, int modCount) {
+            this.current = origin;
+            this.est = size;
+            this.expectedModCount = modCount;
+        }
+
+        @Override
+        public boolean tryAdvance(Consumer<? super E> action) {
+            if (current == null || modCount != expectedModCount) {
+                return false;
+            }
+            action.accept(current.data);
+            current = current.next;
+            est--;
+            return true;
+        }
+
+        @Override
+        public Spliterator<E> trySplit() {
+            // Not implementing splitting; return null means this Spliterator is not splittable
+            return null;
+        }
+
+        @Override
+        public long estimateSize() {
+            return est;
+        }
+
+        @Override
+        public int characteristics() {
+            return SIZED | SUBSIZED | ORDERED | NONNULL;
+        }
     }
 }
